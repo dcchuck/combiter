3548d31d7e9ff00a573a2b2f4ae16db7ee1316d2
Chuck Danielsson <chuck.danielsson@gmail.com>
2022-11-14 22:55:58 -0500

    install linter & prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json']
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
--- src/combinations.ts
+++ src/combinations.ts
@@ -53,4 +53,3 @@ export const combinations = <T>(input: T[], count: number): T[][] => {
 
   return results
 }
-
